9804806c5e018c074c120e78dfa16c4b24e3e40a
Availu <kanade7331@gmail.com>
2014-07-24 13:32:16 +0200

    initial

---
+++ osu!InputDelayRemover/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace osu_InputDelayRemover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Status:";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(59, 19);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(62, 13);
+            this.labelStatus.TabIndex = 1;
+            this.labelStatus.Text = "placeholder";
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Location = new System.Drawing.Point(173, 13);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(90, 24);
+            this.buttonChange.TabIndex = 3;
+            this.buttonChange.Text = "buttonChange";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.buttonChange_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Registry hack find by smoogipooo, program by Avail";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 72);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "osu!InputDelayRemover";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ osu!InputDelayRemover/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.Win32;
+
+namespace osu_InputDelayRemover
+{
+    public partial class Form1 : Form
+    {
+        bool hacked = false;
+
+        string regPath = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers";
+        string file = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            initialPopulation();
+            openTheFile();
+        }
+
+        void initialPopulation()
+        {
+            labelStatus.Text = "Unknown";
+            labelStatus.ForeColor = Color.Yellow;
+            buttonChange.Text = "Enable";
+            buttonChange.ForeColor = Color.LightGray;
+            buttonChange.Enabled = false;
+        }
+
+        void openTheFile()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "osu!|osu!.exe|osu!test|osu!test.exe";
+
+            DialogResult OpenFile = openFileDialog1.ShowDialog();
+            if (OpenFile == DialogResult.OK)
+            {
+                file = openFileDialog1.FileName;
+                isRegistryHackedCheck(file);
+            }
+            else
+            {
+                Application.Exit();
+            }
+        } 
+
+        void isRegistryHackedCheck(string file)
+        {
+            object val = Registry.GetValue(regPath, file, null);
+            if (val == null)
+            {
+                labelStatus.Text = "Not enabled";
+                labelStatus.ForeColor = Color.Red;
+                buttonChange.Text = "Enable";
+                buttonChange.ForeColor = Color.Black;
+                buttonChange.Enabled = true;
+                hacked = false;
+            }
+            else
+            {
+                labelStatus.Text = "Enabled";
+                labelStatus.ForeColor = Color.Green;
+                buttonChange.Text = "Disable";
+                buttonChange.ForeColor = Color.Black;
+                buttonChange.Enabled = true;
+                hacked = true;
+            }
+        }
+
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+            if (hacked == true)
+            {
+                //Registry.LocalMachine.OpenSubKey(regPath).DeleteValue(file, true);
+                RegistryKey regkeyOpen = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers", true);
+                regkeyOpen.DeleteValue(file, true);
+                isRegistryHackedCheck(file);
+            }
+            else
+            {
+                Registry.SetValue(regPath, file, "NoDTToDITMouseBatch");
+                isRegistryHackedCheck(file);
+            }
+        }
+    }
+}
